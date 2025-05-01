d849aa29b72c2f2331bd7db9873a31931041184c
DIVYASRI <120447261+tdivyasri@users.noreply.github.com>
2025-05-01 06:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def predict():
             return render_template('index.html', prediction_text=f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
